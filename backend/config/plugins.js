b4aba201776037507ef3dfffd15dbd98c457a9ef
tejindersinghts75 <50661634+rajinderbarha@users.noreply.github.com>
2024-06-04 16:59:32 +0530

    first deploy to test the demployment on render

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,37 @@
-module.exports = () => ({});
+// module.exports = ({ env }) => ({
+//     // ...
+//     upload: {
+//       config: {
+//         provider: 'cloudinary',
+//         providerOptions: {
+//           cloud_name: env('dr1b4ezct'),
+//           api_key: env('684697795288164'),
+//           api_secret: env('o3i0g1WWQp3jcH88BZvPQVAps7g'),
+//         },
+//         actionOptions: {
+//           upload: {},
+//           delete: {},
+//         },
+//       },
+//     },
+//     // ...
+//   });
+
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
